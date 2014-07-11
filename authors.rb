c2d421a25fcbb7583e45f246d2588a5f53f7034c
teoric <code.teoric@gmail.com>
2014-07-11 22:23:53 +0200

    document, make useful again

--- authors.rb
+++ authors.rb
@@ -1,20 +1,27 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+# use as:
+# $ svn log | authors.rb > authors-transform.txt
+#
+# generates a template for an authors-transform.txt to be used for rewriting
+# the commit history with nice names.
+
 authors = Hash.new(0)
 
 ARGF.each do |line|
-  line = line.force_encoding("cp1252").encode("utf-8")
+  line = line.force_encoding("cp1252").encode("utf-8") # old repository
   if line.match(/(?<=author:\s)\S+(?=;)/)
     # puts $&
     authors[$&] += 1
   end
 end
 
-# authors.each {|k,v|
-#   puts "#{k} = NAME <#{k}@ikp.uni-bonn.de>"
-# }
-
 authors.each {|k,v|
-  puts "#{k}: #{v}"
+  puts "#{k} = NAME <#{k}@SERVER>"
 }
+
+# Count authors instead:
+# authors.each {|k,v|
+#   puts "#{k}: #{v}"
+# }
